e94e1460c2fbfe3476064aeb38786079671d31c4
alessandro.vurro <alessandro.vurro@092d7813-c906-8c0d-5406-876db11828ef>
2015-03-31 14:53:41 +0000

    aggiunti test su classes di JMapAccessor

---
+++ trunk/JMapper Test/src/test/java/com/googlecode/jmapper/integrationtest/configurations/bean/JMapAccSrc3.java
@@ -0,0 +1,21 @@
+package com.googlecode.jmapper.integrationtest.configurations.bean;
+
+public class JMapAccSrc3 {
+
+	private String field;
+
+	public JMapAccSrc3() {}
+	
+	public JMapAccSrc3(String field) {
+		super();
+		this.field = field;
+	}
+
+	public String getSrcField() {
+		return field;
+	}
+
+	public void setSrcField(String field) {
+		this.field = field;
+	}
+}
